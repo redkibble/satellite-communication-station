54deaacee891179993fcfcd17264de9766440770
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-09-02 11:32:21 +0200

    Update __init__.py

--- backend/api/apis/__init__.py
+++ backend/api/apis/__init__.py
@@ -4,6 +4,7 @@ from api.apis.station import router as station_router
 from api.apis.trackedsats import router as trackedsats_router
 from api.apis.radio import router as radio_router
 from api.apis.system import router as system_router
+from api.apis.webbrowser import router as webbrowser_router
 
 apis = APIRouter()
 apis.include_router(satellite_router, prefix="/api/satellite")
@@ -11,4 +12,5 @@ apis.include_router(station_router, prefix="/api/station")
 apis.include_router(trackedsats_router, prefix="/api/trackedsats")
 apis.include_router(radio_router, prefix="/api/radio")
 apis.include_router(system_router, prefix="/api/system")
+apis.include_router(webbrowser_router, prefix= "/api/webbrowser")
 __all__ = ["apis"]
