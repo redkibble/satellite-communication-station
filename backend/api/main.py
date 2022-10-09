343a152b7ba5f6ec9ee0c5b46bdcdeb030366ec8
Chaitanya Mandala <35036701+NicoMandala@users.noreply.github.com>
2022-10-09 18:14:59 +0200

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,18 +13,18 @@ app = FastAPI(
     docs_url="/",
 )
 app.include_router(apis)
-
+"""
 origins = [#"*"
     "",
     "http://localhost:3000",
     "http://localhost",
     "http://localhost:8080",
-]
+]"""
 
 # Sets all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= origins #[str(origin) for origin in []], # changed [] to origins and referring to the list above
+    allow_origins=  [str(origin) for origin in []], # changed [] to origins and referring to the list above
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
